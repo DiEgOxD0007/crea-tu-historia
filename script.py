0c11ae1c2be5427144784032c10c0b0865ae533f
DiEgOxD0007 <119131725+DiEgOxD0007@users.noreply.github.com>
2023-04-15 17:38:24 -0600

    Create script.py

---
+++ script.py
@@ -0,0 +1,34 @@
+print("el juego va a comenzar cabrones >:D")
+
+nombre = input("Ingresa un nombre: ")
+adj1 = input("ingresa un adjetivo")
+adj2= input("ingresa un adjetivo mas xd")
+adj3 = input("ingresa otro adjetivo hdtptm")
+verbo = input("ingresa un verbo xd ")
+sus1 = input("ingrese un sustantivo uwu")
+sus2= input("ingresa otro sustantivo por que si hdtptm xd uwu")
+animal =input("ingresa un animal osea tu :>")
+comida= input("ingresa una comida que sepa a kk xd ")
+fruta = input ("ingresa una puta")
+superheroe = input ("ingresa un superheroe que no existe porque todos somos ezquizofrenicos")
+pais = input("ingresa el pais de tu sueños porque no puedes viajar porque te recuerdo que eres pobre xdddddd")
+postre =input("ingresa un postre que sepa a orines de perro")
+ano=input("ingresa el año de nacimiento uwu xddddd")
+cuentabancaria =input ("ingresa tu numero de cuenta bancaria ¿porque? ¡porque si! xddddd ademas queti :>")
+
+print("Esta mañana {} despertó sintiéndose {}. '¡Será un día {}!' Afuera, un montón de {}s estaban protestando para mantener l@s {}s en las tiendas. Empezaron a {} al ritmo de {}, lo que puso a l@s {}s muy {}s. Preocupad@, {} le mandó un mensaje a {}, que mandó en avión a {} a {} y aventó a {} en un charco congelado de {}. {} despertó en el año {}, en un mundo donde l@s {}s gobernaban el mundo.".format(nombre, adj1, adj2, animal, comida, verbo, sus1, fruta, adj3, nombre, superheroe, nombre, pais, nombre, postre, nombre, ano, sus2))
+
+
+
+
+
+
+
+
+
+#edad = input("ingresa una edad ")
+# print(nombre)
+# print("tiene")
+# print(edad)
+# print("años")
+# print(nombre + " tiene " + edad + " años" " y huele a caca")
